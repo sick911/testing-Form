17b0702a7060a75e52a5299c3ebf8d23206f7644
Koushik <koushikkalgal@gmail.com>
2022-02-08 16:44:46 +0530

    stmpjs and mailtrap

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-function sendEmail() {
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
   Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "therealonestain@gmail.com",
-    Password: "DA5EE55641AE8CEACAE1B7DF41DB5F4651E0",
+    Host: "smtp.mailtrap.io",
+    Username: "97f2fdf1fedb0b",
+    Password: "40cb5cfce89fb8",
     To: "koushikkalgal@gmail.com",
     From: document.getElementById("email").value,
     Subject: "New Contact Form Enquiry",
     Body: document.getElementById("message").value,
   }).then((message) => alert(message));
-}
+});
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "therealonestain@gmail.com",
+//     Password: "DA5EE55641AE8CEACAE1B7DF41DB5F4651E0",
+//     To: "koushikkalgal@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "New Contact Form Enquiry",
+//     Body: document.getElementById("message").value,
+//   }).then((message) => alert(message));
+// }
+// console.log(Email.send.To);
